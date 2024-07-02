2de86ae475e411b30e46205ad8e091d229b3775b
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2024-07-02 13:00:57 -0300

    Commit Andreygabriel
    O presente Commit descreve:
    
    >Alterações:
    
    -Adição - Incorporado a função jogador_turno_atual um laço de repetição que repete as perguntas de escolha das posições caso não esteja dentro das existentes
    -Adição - Iniciado criação da função que verifica se a posicao escolhida é valida ou não
    -Alteração - Alterado variaveis das posições para dicionarios
    
    >Impactos:
    
    - Sucesso na na validação se posicao escolhida é válida
    - Iniciado na função 'verifica_pos_valida' uma condição que verifica se posicao escolhida está ocupada

--- jogo-1.0.py
+++ jogo-1.0.py
@@ -7,67 +7,86 @@ def pontos_partida():
 def status_partida():
     print
 
-def atribui_posicao():
+def atribui_posicao(linha,coluna):
     print
 
-def verifica_pos_val(jog_ult_jogada):
-    print
+def verifica_pos_val(linha, coluna):
+    valido = False
+    if ((linha > 3 or linha < 1) and (coluna > 3 or coluna < 1)):
+        valido = False
+        print('Linha ou coluna fora do intervalo existente!')
+    else:
+        posicao_ocupada = 0
+        for posicao in lista_todas_posicoes:
+            if ((posicao['linha'] == linha) and (posicao['coluna'] == coluna)):
+                posicao_ocupada = posicao['ocupada']
+        if (posicao_ocupada == 1):
+            print('Posição escolhida está ocupada pelo outro jogador!')
+            valido = False
+        else:
+            print('Posição escolhida com sucesso!')
+            valido = True
+    return valido
 
 def jogador_turno_atual():
-    global jogador01
-    global jogador02
+    global jogador_x
+    global jogador_o
     global jog_ult_jogada
-    vez_jogada = ''
     
-    if (jog_ult_jogada == 'jog1'):
-        vez_jogada = jogador02[1]
-        jog_ult_jogada = 'jog2'
+    if (jog_ult_jogada == jogador_x[1]):
+        jog_ult_jogada = jogador_o[1]
     else:
-        vez_jogada = jogador01[1]
-        jog_ult_jogada = 'jog1'
-    input(f'Escolha uma posição {vez_jogada}: ')
-    verifica_pos_val(jog_ult_jogada)
+        jog_ult_jogada = jogador_x[1]
+
+    repete_escolha = False
+    while(repete_escolha == False):
+        linha = 0
+        coluna = 0
+        linha = int(input(f'{jog_ult_jogada}, escolha uma linha: '))
+        coluna = int(input(f'{jog_ult_jogada}, escolha uma coluna: '))
+        repete_escolha = verifica_pos_val(linha, coluna)
+    atribui_posicao(linha, coluna)
     
-#controles globais de jogada
-jog_ult_jogada = 'jog2' #jogador 01 como inicial
-
-
 #controle de posição
-    #indice 0 é para ocupação da posição quando igual a 1
-    #indice 1 é para preenchimento do simbolo inserido
-    #variavel_x_y - X é linha e Y é coluna - valido para os controles de combinação
-posicao_1_1 = [0,'']
-posicao_1_2 = [0,'']
-posicao_1_3 = [0,'']
+posicao_1_1 = {'ocupada':0,'simbolo':'','linha':1,'coluna':1}
+posicao_1_2 = {'ocupada':0,'simbolo':'','linha':1,'coluna':2}
+posicao_1_3 = {'ocupada':0,'simbolo':'','linha':1,'coluna':3}
 
-posicao_2_1 = [0,'']
-posicao_2_2 = [0,'']
-posicao_2_3 = [0,'']
+posicao_2_1 = {'ocupada':0,'simbolo':'','linha':2,'coluna':1}
+posicao_2_2 = {'ocupada':0,'simbolo':'','linha':2,'coluna':2}
+posicao_2_3 = {'ocupada':0,'simbolo':'','linha':2,'coluna':3}
 
-posicao_3_1 = [0,'']
-posicao_3_2 = [0,'']
-posicao_3_3 = [0,'']
+posicao_3_1 = {'ocupada':0,'simbolo':'','linha':3,'coluna':1}
+posicao_3_2 = {'ocupada':0,'simbolo':'','linha':3,'coluna':2}
+posicao_3_3 = {'ocupada':0,'simbolo':'','linha':3,'coluna':3}
+
+lista_todas_posicoes = [posicao_1_1, posicao_1_2, posicao_1_3, posicao_2_1, posicao_2_2, posicao_2_3, posicao_3_1, posicao_3_2, posicao_3_3]
 
 #controle de combinação
-vertical_1_1 = posicao_1_1[0] + posicao_2_1[0] + posicao_3_1[0]
-vertical_1_2 = posicao_1_2[0] + posicao_2_2[0] + posicao_3_2[0]
-vertical_1_3 = posicao_1_3[0] + posicao_2_3[0] + posicao_3_3[0]
+vertical_1_1 = posicao_1_1['ocupada'] + posicao_2_1['ocupada'] + posicao_3_1['ocupada']
+vertical_1_2 = posicao_1_2['ocupada'] + posicao_2_2['ocupada'] + posicao_3_2['ocupada']
+vertical_1_3 = posicao_1_3['ocupada'] + posicao_2_3['ocupada'] + posicao_3_3['ocupada']
 
-horizontal_1_1 = posicao_1_1[0] + posicao_1_2[0] + posicao_1_3[0]
-horizontal_1_2 = posicao_2_1[0] + posicao_2_2[0] + posicao_2_3[0]
-horizontal_1_3 = posicao_3_1[0] + posicao_3_2[0] + posicao_3_3[0]
+horizontal_1_1 = posicao_1_1['ocupada'] + posicao_1_2['ocupada'] + posicao_1_3['ocupada']
+horizontal_1_2 = posicao_2_1['ocupada'] + posicao_2_2['ocupada'] + posicao_2_3['ocupada']
+horizontal_1_3 = posicao_3_1['ocupada'] + posicao_3_2['ocupada'] + posicao_3_3['ocupada']
 
-diagonal_1 = posicao_1_1[0] + posicao_2_2[0] + posicao_3_3[0]
-diagonal_2 = posicao_1_3[0] + posicao_2_2[0] + posicao_3_1[0]
+diagonal_1 = posicao_1_1['ocupada'] + posicao_2_2['ocupada'] + posicao_3_3['ocupada']
+diagonal_2 = posicao_1_3['ocupada'] + posicao_2_2['ocupada'] + posicao_3_1['ocupada']
 
 
 #variáveis de jogadores
-jogador01 = [0,''] #pontuação e nome
-jogador02 = [0,'']
+jogador_x = [0,''] #pontuação e nome
+jogador_o = [0,'']
+
+#controles globais de jogada
+jog_ult_jogada = jogador_o[1] #jogador 01 como inicial
+
 
 #programa principal
-jogador01[1] = input('Digite o nome do primeiro jogador: ')
-jogador02[1] = input('Digite o nome do segundo jogador: ')
+jogador_x[1] = input('Nome do Jogador que será o X: ')
+jogador_o[1] = input('Nome do Jogador que será o O: ')
+print(f'{jogador_x[1]} inicia a partida!')
 input('Enter para iniciar')
 
 while(True):
