6636ade643c27d52c3b44e446b693461b02e5734
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2024-07-01 22:08:59 -0300

    Commit Andreygabriel
    O presente Commit descreve:
    
    >Alterações:
    
    -Adição - Planejado e criado todas as funções que o sistema utilizará
    -Adição - Criado primeira parte da função "jogador_turno_atual" que rotaciona entre os jogadores conforme ultimo jogador que jogou
    -Adição - Criado pequeno menu que coleta nomes dos jogadores para fins de testes da função citada acima
    
    >Impactos:
    
    -Sucesso no algoritmo de troca de jogador atual do turno

--- jogo-1.0.py
+++ jogo-1.0.py
@@ -1,8 +1,41 @@
-#variavel_x_y - X é linha e Y é coluna
+def turno_computador():
+    print
+
+def pontos_partida():
+    print
+
+def status_partida():
+    print
+
+def atribui_posicao():
+    print
+
+def verifica_pos_val(jog_ult_jogada):
+    print
+
+def jogador_turno_atual():
+    global jogador01
+    global jogador02
+    global jog_ult_jogada
+    vez_jogada = ''
+    
+    if (jog_ult_jogada == 'jog1'):
+        vez_jogada = jogador02[1]
+        jog_ult_jogada = 'jog2'
+    else:
+        vez_jogada = jogador01[1]
+        jog_ult_jogada = 'jog1'
+    input(f'Escolha uma posição {vez_jogada}: ')
+    verifica_pos_val(jog_ult_jogada)
+    
+#controles globais de jogada
+jog_ult_jogada = 'jog2' #jogador 01 como inicial
+
 
 #controle de posição
     #indice 0 é para ocupação da posição quando igual a 1
     #indice 1 é para preenchimento do simbolo inserido
+    #variavel_x_y - X é linha e Y é coluna - valido para os controles de combinação
 posicao_1_1 = [0,'']
 posicao_1_2 = [0,'']
 posicao_1_3 = [0,'']
@@ -27,8 +60,15 @@ horizontal_1_3 = posicao_3_1[0] + posicao_3_2[0] + posicao_3_3[0]
 diagonal_1 = posicao_1_1[0] + posicao_2_2[0] + posicao_3_3[0]
 diagonal_2 = posicao_1_3[0] + posicao_2_2[0] + posicao_3_1[0]
 
-#teste
-posicao_1_1[0] = 1
-posicao_1_1[1] = 'X'
 
-print(posicao_1_1[:])
+#variáveis de jogadores
+jogador01 = [0,''] #pontuação e nome
+jogador02 = [0,'']
+
+#programa principal
+jogador01[1] = input('Digite o nome do primeiro jogador: ')
+jogador02[1] = input('Digite o nome do segundo jogador: ')
+input('Enter para iniciar')
+
+while(True):
+    jogador_turno_atual()
